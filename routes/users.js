60fffc2a9310b4f6cc547d5a90d13fe9efa1be96
NatCanCode <77299658+NatCanCode@users.noreply.github.com>
2023-09-03 22:29:24 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,24 +3,30 @@ const router = express.Router();
 const { User } = require("../db.js");
 
 // sequelize model:generate --name User --attributes user_role:string,firstname:string,lastname:string,email:string,phone:string,user_password:string
-/* /users */ 
+// Then delete test attibute in migration file
+
+/* Users */
+/* GET */
 router.get('/users', function(req, res, next) {
     res.json({
       message: {email: 'alex@mail.com', firstname: 'Alex', lastname: 'Zerah',}
     });
-  });
-  
-  router.post('/users', function(req, res, next) {
+});
+
+/* POST */
+router.post('/users', function(req, res, next) {
     res.json({message: 'Your account has been created'});
-  });
-  
-  router.put('/users', function(req, res, next) {
+});
+
+/* PUT */
+router.put('/users', function(req, res, next) {
     res.json({message: 'Your account has been updated'});
-  });
-  
-  router.delete('/users', function(req, res, next) {
+});
+
+/* DELETE */
+router.delete('/users', function(req, res, next) {
     res.json({message: 'Your account has been deleted'});
-  });
+});
   
 
 module.exports = router;
