25f9e0dad3779bca15e9e01e6d9e643230209b17
NatCanCode <77299658+NatCanCode@users.noreply.github.com>
2023-09-03 22:28:55 +0200

    Update spots.js

--- routes/spots.js
+++ routes/spots.js
@@ -5,25 +5,29 @@ const { Spot } = require("../db.js");
 // sequelize model:generate --name Spot --attributes test:string
 // Then delete test attibute in migration file
 
-  /* /spots */
-  router.get('/spots', function(req, res, next) {
+/* Spots */
+/* GET */
+router.get('/spots', function(req, res, next) {
     res.json({
       message: {id: 1}
     })
-  });
-  
-  router.post('/spots', function(req, res, next) {
+});
+
+/* POST */
+router.post('/spots', function(req, res, next) {
     res.json({message: 'Your table has been created'});
-  });
-  
-  router.put('/spots', function(req, res, next) {
+});
+
+/* PUT */
+router.put('/spots', function(req, res, next) {
     res.json({message: 'Your table has been updated'});
-  });
-  
-  router.delete('/spots', function(req, res, next) {
+});
+
+/* DELETE */
+router.delete('/spots', function(req, res, next) {
     res.json({message: 'Your table has been deleted'});
   }
-  );
+);
 
 
 module.exports = router;
