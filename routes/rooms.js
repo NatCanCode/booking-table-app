3f8430d3ebfb8c3afdc5fe7d31a87f987667e065
NatCanCode <77299658+NatCanCode@users.noreply.github.com>
2023-09-03 22:28:26 +0200

    Update rooms.js

--- routes/rooms.js
+++ routes/rooms.js
@@ -4,24 +4,29 @@ const { Room } = require("../db.js");
 
 // sequelize model:generate --name Room --attributes test:string
 // Then delete test attibute in migration file
-  /* /rooms */
-  router.get('/rooms', function(req, res, next) {
+
+/* Rooms */
+/* GET */
+router.get('/rooms', function(req, res, next) {
     res.json({
       message: {id: 1}
     })
-  });
-  
-  router.post('/rooms', function(req, res, next) {
+});
+
+/* POST */
+router.post('/rooms', function(req, res, next) {
     res.json({message: 'Your room has been created'});
-  });
-  
-  router.put('/rooms', function(req, res, next) {
+});
+
+/* PUT */
+router.put('/rooms', function(req, res, next) {
     res.json({message: 'Your room has been updated'});
-  });
-  
-  router.delete('/rooms', function(req, res, next) {
+});
+
+/* DELETE */
+router.delete('/rooms', function(req, res, next) {
     res.json({message: 'Your room has been deleted'});
-  });
+});
   
 
 module.exports = router;
